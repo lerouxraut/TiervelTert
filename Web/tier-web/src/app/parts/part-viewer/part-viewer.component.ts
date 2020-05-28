754f27d9c2adc66c121b57ba1c3337bed278f823
lerouxraut <lerouxraut@gmail.com>
2020-05-28 21:58:09 +0200

    stl viewer added

---
+++ Web/tier-web/src/app/parts/part-viewer/part-viewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-part-viewer',
+  templateUrl: './part-viewer.component.html',
+  styleUrls: ['./part-viewer.component.css']
+})
+export class PartViewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Web/tier-web/src/app/parts/parts.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PartViewerComponent } from './part-viewer/part-viewer.component';
+import { StlModelViewerModule  } from 'angular-stl-model-viewer';
+
+
+@NgModule({
+  declarations: [PartViewerComponent],
+  imports: [
+    StlModelViewerModule,
+    CommonModule
+  ],
+  exports: [
+    PartViewerComponent,
+  ]
+})
+export class PartsModule { 
+
+}
